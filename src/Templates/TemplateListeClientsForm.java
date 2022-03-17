64105c14b6711e60e53dcbccebd19abff6ca8bcd
realbenben <b.patou59@gmail.com>
2022-03-17 12:32:44 +0100

    create liste clt

---
+++ src/Templates/TemplateListeClientsForm.java
@@ -0,0 +1,52 @@
+package Templates;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+import javax.swing.JToolBar;
+import javax.swing.JLayeredPane;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import java.awt.CardLayout;
+
+public class TemplateListeClientsForm extends JFrame {
+
+	private JPanel background;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TemplateListeClientsForm frame = new TemplateListeClientsForm();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TemplateListeClientsForm() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 1000);
+		background = new JPanel();
+		background.setBackground(new Color(0, 0, 102));
+		background.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
+		setContentPane(background);
+		background.setLayout(new CardLayout(0, 0));
+		
+	}
+
+}
