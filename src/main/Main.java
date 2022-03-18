ceabd1dd28dbc9ed2c794c06acb33896e795d038
charly <charly.dewalles@gmail.com>
2022-03-18 10:16:54 +0100

    ff

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
 		
     	AuthAndGetClients auth = new AuthAndGetClients(); 
     	int idConseiller = auth.AuthAndGetClient();
