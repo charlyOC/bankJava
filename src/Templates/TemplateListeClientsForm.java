ab382216346c971fc5e10f0e34f9fc92b012527e
realbenben <b.patou59@gmail.com>
2022-03-18 14:14:27 +0100

    add login T

--- src/Templates/TemplateListeClientsForm.java
+++ src/Templates/TemplateListeClientsForm.java
@@ -1,25 +1,18 @@
 package Templates;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 import java.awt.Color;
-import javax.swing.JInternalFrame;
-import javax.swing.JToolBar;
-import javax.swing.JLayeredPane;
-import java.awt.FlowLayout;
 import java.awt.Component;
-import java.awt.CardLayout;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Cursor;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class TemplateListeClientsForm extends JFrame {
 
